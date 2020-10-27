4cae51b168027d67758615b80a89b79566cfcdbd
little <little500ml@gmail.com>
2020-10-27 10:34:26 +0900

    横選択追加、次歯抜け対応

--- Assets/CursorController.cs
+++ Assets/CursorController.cs
@@ -6,35 +6,49 @@ using UnityEngine.UI;
 public class CursorController : MonoBehaviour {
     public Transform Cursor;
     public Text SelectText;
+    [SerializeField] private List<Transform> Command = new List<Transform>();
 
-    int CursorPoint;
-    const int DefaultPos = 219;
-    const int CursorOffset = 100;
-    enum CommandList {
-        ATTACK,
-        DEFENCE,
-        MAGIC,
-        ITEM,
-        COMANDMAX
+    Vector2Int CursorPoint;
+    const float DefaultPosX = -290;
+    const float DefaultPosY = 80;
+    const float OffsetX = 300;
+    const float OffsetY = 100;
+    const int CommandUpDownMax = 4;
+    const int CommandLeftRightMax = 2;
+
+
+    private void Start() {
+        var CommandField = GameObject.Find("CommandField").transform;
+
+        foreach (Transform child in CommandField) {
+            Command.Add(child);
+        }
+
     }
 
 
     void Update() {
         Vector3 pos = Cursor.localPosition;
 
-
         if (Input.GetKeyDown(KeyCode.Z)) {
-            SelectText.text = "Select：" + (CommandList)CursorPoint;
+            SelectText.text = Command[CursorPoint.x + CursorPoint.y].GetComponent<Text>().text + "を選択";
+
         }
         if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            CursorPoint = (CursorPoint + (int)CommandList.COMANDMAX - 1) % (int)CommandList.COMANDMAX;
-            pos.y = DefaultPos - CursorOffset * CursorPoint;
+            CursorPoint.y = (CursorPoint.y + (CommandUpDownMax - 1)) % CommandUpDownMax;
+            pos.y = DefaultPosY - OffsetY * CursorPoint.y;
         }
         if (Input.GetKeyDown(KeyCode.DownArrow)) {
-           
-            CursorPoint = (CursorPoint + 1) % (int)CommandList.COMANDMAX;
-            pos.y = DefaultPos - CursorOffset * CursorPoint;
+            CursorPoint.y = (CursorPoint.y + 1) % CommandUpDownMax;
+            pos.y = DefaultPosY - OffsetY * CursorPoint.y;
         }
+        if (Input.GetKeyDown(KeyCode.RightArrow) || Input.GetKeyDown(KeyCode.LeftArrow)) {
+            CursorPoint.x = (CursorPoint.x + CommandUpDownMax) % (CommandUpDownMax * CommandLeftRightMax);
+            if (CursorPoint.x < CommandUpDownMax) pos.x = DefaultPosX;
+            else pos.x = DefaultPosX + OffsetX;
+
+        }
+
         Cursor.localPosition = pos;
     }
 }
